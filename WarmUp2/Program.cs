a3e11091c31c9c4328eafb07ab6f290dd9040c5d
Youngs285 <nitronite500@gmail.com>
2014-04-30 09:24:29 -0400

    WarmUp2

---
+++ WarmUp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarmUp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter A Number: ");
+                int n;
+                string input = Console.ReadLine();
+
+                n = Convert.ToInt32(input);
+
+                Console.Write("W");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write('o');
+                }
+                Console.WriteLine("W!");
+            }
+        }
+    }
+}
